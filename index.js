597675c16e9ca1243e427ae9fa1dd675cbe5e9ba
rhutiksahu12 <shubhamsahu938@gmail.com>
2023-08-08 01:04:02 +0200

    design finished

---
+++ index.js
@@ -0,0 +1,8 @@
+let bundle = document.getElementsByName('bundlebtn');
+console.log(bundle[1].checked)
+
+for (i =0; i<bundle.length; i++){
+    bundle[i].addEventListener('change', ()=>{
+        
+    })
+}
