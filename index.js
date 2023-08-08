4754a8ba2912c18a3eae06a5d3ff97d0ba83a027
rhutiksahu12 <shubhamsahu938@gmail.com>
2023-08-08 02:53:21 +0200

    finalised

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 let bundle = document.getElementsByName('bundlebtn');
-console.log(bundle[1].checked)
+// console.log(bundle[1].checked)
+let collapsablediv = document.querySelectorAll('.collapse');
+// console.log(collapsable[1])
+console.log(collapsablediv.length)
 
-for (i =0; i<bundle.length; i++){
-    bundle[i].addEventListener('change', ()=>{
-        
+bundle.forEach(radio =>{
+    radio.addEventListener('change', ()=>{
+        const divToShow =radio.value;
+
+        collapsablediv.forEach(div => {
+            if(div.id===divToShow){
+                div.style.display = 'block'
+                div.parentElement.classList.add('active')
+            } else {
+                div.style.display='none'
+                div.parentElement.classList.remove('active')
+            }
+
+        })
     })
-}
+})
+    
